3c88582f2290ffb0dd890d0da7ab6974531040e6
Sebas <99556112+AstroGaze@users.noreply.github.com>
2025-05-26 01:18:34 -0600

    Cyberpuerta Scraper headless to true

--- scrapers/cyberpuertaScraper.js
+++ scrapers/cyberpuertaScraper.js
@@ -11,7 +11,7 @@ async function searchCyberpuerta(productName) {
 
   try {
     browser = await playwright.chromium.launch({
-      headless: false,
+      headless: true,
       args: ["--no-sandbox", "--disable-setuid-sandbox"], // Add security flags for production
     });
     const context = await browser.newContext({
